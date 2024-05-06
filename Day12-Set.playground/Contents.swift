17ffa9b648da4de35d5a6044707b25bcf4c1e461
beifengchuiluoyanlei <1752440588@qq.com>
2024-05-06 21:56:27 +0800

    Set

--- Day12-Set.playground/Contents.swift
+++ Day12-Set.playground/Contents.swift
@@ -2,6 +2,47 @@ import UIKit
 
 var greeting = "Hello, playground"
 
+// Set的概念
+/// set是指某种具有特定性质的具体的或抽象的对象汇总而成的集体。其中，构成Set的这些对象则称为该Set的元素
+
+// 集合的三个特性
+/// 确定性：给定一个集合，任给一个元素，该元素或者属于或者不属于该集合，二者必居其一
+/// 互斥性：一个集合中，任何两个元素都认为是不相同的，即每个元素只能出现一次
+/// 无序性：一个集合中，每个元素的地位都是相同的，元素之间是无序的
+
+/// Swift的集合类型写作Set<Element>，这里的Element是Set要存储的类型。不同于数组，集合没有等价的简写
+
+// 创建Set
+/// 使用初始化器语法来创建一个确定类型的空Set
+/// 使用数组字面量创建Set
+
+var letters = Set<Character>()
+letters.insert("c")
+print(letters)
+
+var course: Set<String> = ["Math", "English", "History"]
+course.insert("History")
+print(course)
+
+// Set类型的哈希值
+/// 为了能让类型存储在Set中，它必须是可哈希的--就是说类型必须提供它计算自身哈希值的方法
+/// 所有的Swift的基础类型（比如String,Int,Double和Bool）默认都是可哈希的，并且可以用于Set或者Dictionary的键
+
+/// 自定义类型需要实现Hashable协议
+struct Person {
+    var name: String
+    var age: Int
+}
+
+extension Person: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(age)
+    }
+}
+
+var personSet = Set<Person>()
+personSet.insert(Person(name: "zhangsan", age: 28))
 
 
 // 访问和修改Set
