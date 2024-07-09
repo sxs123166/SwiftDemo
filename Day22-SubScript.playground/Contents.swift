348bc098af123e90a06683781f6a82385872b4b1
shangxuesong_dxm <shangxuesong@duxiaoman.com>
2024-07-09 10:11:22 +0800

    提交描述：下标和下表重载 修改内容： 影响范围： BUG编号：

--- Day22-SubScript.playground/Contents.swift
+++ Day22-SubScript.playground/Contents.swift
@@ -17,3 +17,43 @@ struct TimesTable {
 }
 let threeTimesTable = TimesTable(multiplier: 3)
 print("six times three is \(threeTimesTable[6])")
+
+// 下标可以接受任意数量的输入形式参数，并且这些输入形式参数可以是任意类型。下标也可以返回任意类型，下标可以使用变量形式参数和可变形式参数，但是不能使用输入输出形式参数或提供默认形式参数值
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        self.grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+
+// 类型下标
+// 实例下标，如果上文描述的那样，你在对应类型的实例上调用下标。你同样也可以定义类型本身的下标。这类下标叫做类型下标。你可通过在subscript关键字前加static关键字来标记类型下标。在类里使用class关键字，这样可以允许子类重写父类的下标实现。
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter,saturn,uranus,neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
+    }
+}
+let mars = Planet[4]
+print(mars)
+
